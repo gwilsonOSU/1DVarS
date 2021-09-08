2c87910032e13f18c63fc7407e95a162afea1dc9
Greg Wilson <wilsongr@oregonstate.edu>
2021-09-07 17:56:21 -0700

    added Silva lab test case with currents

---
+++ test_cases/silva2011_withcurrent/vanderA2013/physicalConstants.m
@@ -0,0 +1,12 @@
+%
+% physical constants shared amongst various sediment transport codes
+%
+
+g=9.8126;
+rho=1030;   % density of water, kg/m3
+rhop=2650;  % density of sediment, kg/m3
+rhoa=1;     % air density, kg/m3
+psed=0.4;  % porosity
+nu=1.7e-6;  % kinematic viscosity of water, m2/s
+s=rhop/rho;
+hmin=0.5;
