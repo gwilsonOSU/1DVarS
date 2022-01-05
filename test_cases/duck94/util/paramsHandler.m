a746f4f07d6364e41b21a593692696a7a602b196
Greg Wilson <wilsongr@oregonstate.edu>
2022-01-05 14:52:04 -0800

    bugs fixed, should have been committed earlier

--- test_cases/duck94/util/paramsHandler.m
+++ test_cases/duck94/util/paramsHandler.m
@@ -1,12 +1,12 @@
-function [v1o,v2o,v3o,v4o,v5o,v6o,v7o,v8o]=paramsHandler(unpack,sedmodel,vi)
+function [v1o,v2o,v3o,v4o,v5o,v6o,v7o,v8o,v9o]=paramsHandler(unpack,sedmodel,vi)
 %
 % USAGE-STYLE-1:
 %
-%   [fv,ks,n,m,xi,alpha,Cc,Cf]=paramsHandler(1,'vanderA',params)
+%   [fv,ks,lambda,n,m,xi,alpha,Cc,Cf]=paramsHandler(1,'vanderA',params)
 %
 % USAGE-STYLE-2:
 %
-%   params=paramsHandler(1,'vanderA',[fv ks n m xi alpha Cc Cf])
+%   params=paramsHandler(1,'vanderA',[fv ks lambda n m xi alpha Cc Cf])
 %
 % Helper function to unpack and re-pack params struct for van der A model.
 % This was needed as a hack to avoid "cannot be classified" error when using
@@ -16,50 +16,53 @@ function [v1o,v2o,v3o,v4o,v5o,v6o,v7o,v8o]=paramsHandler(unpack,sedmodel,vi)
 % example above is for sedmodel=='vanderA'.
 %
 
-if(isvector(vi))
+if(length(vi)>1)
   v1i=vi(1);
   v2i=vi(2);
   v3i=vi(3);
   v4i=vi(4);
   v5i=vi(5);
   v6i=vi(6);
-  if(length(vi)>6)  % vanderA has 8 params
-    v7i=vi(7);
+  v7i=vi(7);
+  if(length(vi)>7)  % vanderA has 9 params
     v8i=vi(8);
+    v9i=vi(9);
   end
-
+end
 if(unpack)
-  params=v1i;
+  params=vi;
   v1o=params.fv   ;
   v2o=params.ks   ;
+  v3o=params.lambda;
   if(strcmp(sedmodel,'vanderA'))
-    v3o=params.n    ;
-    v4o=params.m    ;
-    v5o=params.xi   ;
-    v6o=params.alpha;
-    v7o=params.Cc   ;
-    v8o=params.Cf   ;
+    v4o=params.n    ;
+    v5o=params.m    ;
+    v6o=params.xi   ;
+    v7o=params.alpha;
+    v8o=params.Cc   ;
+    v9o=params.Cf   ;
   elseif(strcmp(sedmodel,'dubarbier'))
-    v3o=params.Cw;
-    v4o=params.Cc;
-    v5o=params.Cf;
-    v6o=params.Ka;
+    v4o=params.Cw;
+    v5o=params.Cc;
+    v6o=params.Cf;
+    v7o=params.Ka;
   end
 else
   params.fv=v1i;
   params.ks=v2i;
+  params.lambda=v3i;
   if(strcmp(sedmodel,'vanderA'))
-    params.n    =v3i;
-    params.m    =v4i;
-    params.xi   =v5i;
-    params.alpha=v6i;
-    params.Cc   =v7i;
-    params.Cf   =v8i;
+    params.n    =v4i;
+    params.m    =v5i;
+    params.xi   =v6i;
+    params.alpha=v7i;
+    params.Cc   =v8i;
+    params.Cf   =v9i;
   elseif(strcmp(sedmodel,'dubarbier'))
-    params.Cw=v3i;
-    params.Cc=v4i;
-    params.Cf=v5i;
-    params.Ka=v6i;
+    params.Cw=v4i;
+    params.Cc=v5i;
+    params.Cf=v6i;
+    params.Ka=v7i;
   end
   v1o=params;
 end
